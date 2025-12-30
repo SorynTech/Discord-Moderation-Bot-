5e248611d730ff8551c64d59ae0858c6af6dd4b4
Soryn Tech <zippydrawzstudioz@gmail.com>
2025-12-30 02:40:09 +0000

    Removed the Logger

--- Moderationbot.py
+++ Moderationbot.py
@@ -23,10 +23,6 @@ bot_owner_sleeping = False
 
 
 
-# Create handler
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 intents = discord.Intents.default()
 intents.message_content = True
